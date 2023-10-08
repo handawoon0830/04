8092349a55d94a12e482ac182ea175915723dd38
handawoon0830 <handawoon0830@naver.com>
2023-10-08 16:34:59 +0900

    5

--- main.c
+++ main.c
@@ -4,15 +4,21 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a;
+	unsigned int x;
+	int b;
 	
-	printf("enter the year :");
-	scanf("%i",&a);
+	printf("input a number :");
+	scanf("%ui", &x);
 	
-	if (a%4==0 && a%100!=0 || a%400==0)
-		printf("the year is À±³â");
-		
-	else
-		printf("the year is not À±³â");
+	for (b=0 ; x !=0 ; x>>=1)
+	{
+		if (x&1)
+		{
+			b++;
+		}
+	}
+	
+	printf("The result is : %i\n",b);
+	
 	return 0;
 }
